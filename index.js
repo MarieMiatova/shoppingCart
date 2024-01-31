c592dc4eacf39b601dcd2482a67bd6292116179a
MarieMiatova <146247021+MarieMiatova@users.noreply.github.com>
2024-01-31 14:03:19 +0300

    Initial commit

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-// создаем корзину
+// шаг 1. создаем корзину
 let shoppingCart = {
     items: [],
     total: 0,
   
-    // добавляем товар
+    // шаг 2. добавляем товар
     addItem: function(name, price, quantity) {
       this.items.push({name, price, quantity});
       this.calculateTotal();
     },
   
-    // удаляем товар
+    // шаг 3. удаляем товар
     removeItem: function(name) {
       this.items = this.items.filter(item => item.name !== name);
       this.calculateTotal();
     },
   
-    // обновляем кол-во товаров
+    // шаг 4. обновляем кол-во товаров
     updateQuantity: function(name, newQuantity) {
       this.items.forEach(item => {
         if (item.name === name) {
@@ -25,7 +25,7 @@ let shoppingCart = {
       this.calculateTotal();
     },
   
-    // общая стоимость
+    // шаг 5. считаем общую стоимость
     calculateTotal: function() {
       this.total = 0;
       for (let item of this.items) {
@@ -33,7 +33,7 @@ let shoppingCart = {
       }
     },
   
-    // удаленяем все товары из корзины
+    // шаг 6. удаленяем все товары из корзины
     clearCart: function() {
       this.items = [];
       this.total = 0;
